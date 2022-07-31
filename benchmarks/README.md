bd7f001ea84a5704ba7e65e9a9fbb64811781d12
Kamil Giszczak <beerkg@gmail.com>
2022-07-31 14:44:31 +0200

    Add benchmarks

---
+++ benchmarks/README.md
@@ -0,0 +1,39 @@
+# Benchmarks
+
+Comparison fo this gem with [Tomlrb](https://github.com/fbernier/tomlrb)
+and [toml-rb](https://github.com/emancu/toml-rb).
+
+It beanchmarks parsing and generating three files:
+
+* `examples/small.toml` (100B)
+* `examples/default.toml` (~5KB)
+* `examples/big.toml` (~1MB)
+
+## Usage
+
+Install all the dependencies:
+
+    $ bundle install
+
+and run benchmarks:
+
+    $ ruby benchmarks.rb
+
+## Results
+
+### Parsing (i/s)
+
+|gem|small.toml|default.toml|big.toml|
+|---|----------|------------|--------|
+|Tomlib|176.838k|6.577k|11.392|
+|Tomlrb|10.697k (16.53x slower)|416.441 (15.79x slower)|1.211 (9.40x slower)
+|toml-rb|810.809 (218.10x slower)|20.683 (318.01x slower)|0.080 (142.09x slower)
+
+### Generating (i/s)
+
+Note - Tomlrb gem doesn't support generating TOML documents.
+
+|gem|small.toml|default.toml|big.toml|
+|---|----------|------------|--------|
+|Tomlib|137.698k|4.741k|22.785|
+|toml-rb|64.312k (2.14x slower)|2.672k (1.77x slower)|0.129 (175.98x slower)
---
+++ benchmarks/benchmarks.rb
@@ -0,0 +1,55 @@
+require 'bundler/setup'
+require 'benchmark/ips'
+require 'bigdecimal'
+
+require 'tomlrb'
+require 'toml-rb'
+require 'tomlib'
+
+examples = {
+  'small' => File.read('examples/small.toml'), # 100B
+  'default' => File.read('examples/default.toml'), # ~5KB
+  'big' => File.read('examples/big.toml'), # ~1MB
+}
+
+puts "\n=========================== parsing ===========================\n\n"
+
+examples.each do |name, data|
+  puts "\n<<<------------------------ #{name} document ------------------------>>>\n\n"
+
+  Benchmark.ips do |x|
+    x.report('tomlrb') do
+      Tomlrb.parse(data)
+    end
+
+    x.report('toml-rb') do
+      TomlRB.parse(data)
+    end
+
+    x.report('tomlib') do
+      Tomlib.load(data)
+    end
+
+    x.compare!
+  end
+end
+
+puts "\n\n\n=========================== generating ===========================\n\n"
+
+examples.each do |name, data|
+  puts "\n<<<------------------------ #{name} document ------------------------>>>\n\n"
+
+  hash = Tomlib.load(data)
+
+  Benchmark.ips do |x|
+    x.report('toml-rb') do
+      TomlRB.dump(hash)
+    end
+
+    x.report('tomlib') do
+      Tomlib.dump(hash, indent: false)
+    end
+
+    x.compare!
+  end
+end
